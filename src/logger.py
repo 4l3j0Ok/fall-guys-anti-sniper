1184571eb1e0c8d611500b3fa837ffffc1dce3e5
AlejoSarmiento <asarmiento@leafnoise.io>
2022-05-24 06:46:32 -0300

    Logger mini-improvement.

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ logging.basicConfig(
 	filename='application.log',
 	encoding='utf-8',
 	level=logging.DEBUG,
-	format="%(asctime)s - %(levelname)s - %(message)s",
+	format="%(asctime)s MODULE: %(module)s (%(funcName)s) - LINE: %(lineno)d - %(levelname)s - %(message)s",
 	datefmt='%d/%m/%Y %H:%M:%S'
 	)
 
