9e12d31e6005c4664c4bb57b5a4a8a04d32b0d43
AlejoSarmiento <asarmiento@leafnoise.io>
2022-05-26 23:31:09 -0300

    Clear and remove button logic

--- src/main.py
+++ src/main.py
@@ -50,23 +50,34 @@ class HomeWindow(QMainWindow):
 
 
 	def ui_basic_config(self):
-		self.setWindowIcon(QIcon(config.ICON_PATH))
+		self.windowIcon = QIcon(config.ICON_PATH)
+		self.setWindowIcon(self.windowIcon)
 		self.action_about.triggered.connect(self.show_about)
 		self.clear_blacklist_button.clicked.connect(self.clear_blacklist)
+		self.remove_player_blacklist_button.clicked.connect(self.remove_player_blacklist)
 
 	def show_about(self):
 		msg_box = QMessageBox()
 		msg_box.setWindowTitle("Acerca de")
+		msg_box.setWindowIcon(self.windowIcon)
 		msg_box.setIcon(QMessageBox.Question)
 		msg_box.setDefaultButton(QMessageBox.Close)
 		msg_box.setText(config.ABOUT_STRING)
-		msg_box.setFixedSize(500, 500)
 		msg_box.exec_()
+
 
 	def clear_blacklist(self):
 		msg_box = QMessageBox()
-		msg_box.setWindowTitle("Limpiar lista negra.")
+		msg_box.setWindowTitle("Limpiar lista negra")
+		msg_box.setWindowIcon(self.windowIcon)
+		if not self.blacklist_list:
+			msg_box.setIcon(QMessageBox.Information)
+			msg_box.setText("La lista negra está vacía.")
+			msg_box.setDefaultButton(QMessageBox.Close)
+			msg_box.exec_()
+			return
 		msg_box.setIcon(QMessageBox.Warning)
+		msg_box.setWindowIcon(self.windowIcon)
 		msg_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
 		msg_box.setText("¿Estás seguro de querer limpiar la lista negra?")
 		msg_box.setInformativeText("Esta acción no se puede deshacer.")
@@ -76,6 +87,36 @@ class HomeWindow(QMainWindow):
 			logger.info("Limpiando la blacklist.")
 			self.blacklist_list.clear()
 			utils.clear_blacklist()
+
+
+	def remove_player_blacklist(self):
+		msg_box = QMessageBox()
+		msg_box.setWindowTitle("Eliminar jugador de la lista negra")
+		msg_box.setWindowIcon(self.windowIcon)
+		if not self.blacklist_list:
+			msg_box.setIcon(QMessageBox.Information)
+			msg_box.setText("La lista negra está vacía.")
+			msg_box.setDefaultButton(QMessageBox.Close)
+			msg_box.exec_()
+			return
+		if not self.blacklist_list.currentItem():
+			msg_box.setIcon(QMessageBox.Information)
+			msg_box.setText("Seleccione un jugador a eliminar.")
+			msg_box.setDefaultButton(QMessageBox.Close)
+			msg_box.exec_()
+			return
+		player_index = self.blacklist_list.currentRow()
+		player_name = self.blacklist_list.currentItem().text()
+		msg_box.setIcon(QMessageBox.Warning)
+		msg_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+		msg_box.setText("¿Estás seguro de que quieres eliminar a '{}' de la lista negra?".format(player_name))
+		msg_box.setInformativeText("Esta acción no se puede deshacer.")
+		msg_box.setFixedSize(500, 500)
+		result = msg_box.exec_()
+		if result == QMessageBox.Yes:
+			logger.info("Limpiando la blacklist.")
+			self.blacklist_list.takeItem(player_index)
+			utils.clear_blacklist(player=player_name)
 
 
 	def run_daemon(self):
